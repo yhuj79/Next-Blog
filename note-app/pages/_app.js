a18008cb98b741fb8dcbcceb3270ad6611025814
yhuj79 <yu1085@naver.com>
2022-10-22 12:09:22 +0900

    photos / bootstrap test

--- note-app/pages/_app.js
+++ note-app/pages/_app.js
@@ -1,7 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
--- note-app/pages/photos.js
+++ note-app/pages/photos.js
@@ -2,9 +2,46 @@ import HeadInfo from "../components/HeadInfo";
 
 function photos() {
   return (
-    <div>
+    <div className="d-flex justify-content-center align-items-center">
       <HeadInfo title="Photos" />
-      <h1>My photos</h1>
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              The useEffect() Hook in React is used to instruct our React
+              components that they need to do something after rendering, and in
+              this scenario, we’d use it to import the bundled Bootstrap
+              JavaScript file.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
