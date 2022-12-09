71df91f449550f223522f69c9155fcc75fea8eb9
yhuj79 <yu1085@naver.com>
2022-12-09 22:49:23 +0900

    getStaticProps

--- note-app/pages/apitest.js
+++ note-app/pages/apitest.js
@@ -3,17 +3,54 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-    const allFoods = await prisma.posting.findMany();
-    console.log(allFoods);
-  }
+export async function getStaticProps() {
+  const posting = await prisma.posting.findMany();
 
-export default function apitest() {
-    console.log(Posting())
+  return {
+    props: {
+      posting: JSON.parse(JSON.stringify(posting)),
+    },
+  };
+}
+
+// export async function add() {
+//   try {
+//     const res = await prisma.user.createMany({
+//       data: {
+//         title: "elsa@prisma.io",
+//         content: "Elsa Prisma! Elsa Prisma! Elsa Prisma!",
+//       },
+//     });
+
+//     console.log("Created a user sucessfully", res);
+//   } catch (err) {
+//     console.log("Create a user Error:", err);
+//   } finally {
+//     async () => {
+//       await prisma.$disconnect();
+//     };
+//   }
+// }
+
+export default function apitest({ posting }) {
+  console.log(JSON.stringify(posting, null, 5));
+
   return (
     <div>
-      <h1>apitest</h1>
-      {/* <p>{JSON.stringify(Posting())}</p> */}
+      <div style={{ display: "flex" }}>
+        <h1>apitest</h1>
+        <button
+          onClick={() => add()}
+          style={{ backgroundColor: "black", margin: "10px" }}
+        >
+          Add
+        </button>
+      </div>
+      <textarea
+        defaultValue={JSON.stringify(posting, null, 5)}
+        rows="70"
+        cols="140"
+      />
     </div>
   );
 }
