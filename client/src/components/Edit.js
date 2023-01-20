9e8b8c899d21156bce967a43a179cc7d4d97ae4d
yhuj79 <yu1085@naver.com>
2023-01-21 02:55:58 +0900

    Modal Test

--- client/src/components/Edit.js
+++ client/src/components/Edit.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { Icon } from "semantic-ui-react";
+import { Button, Header, Icon, Modal } from "semantic-ui-react";
 import styles from "../../styles/Edit.module.css";
 
 export default function Edit({ id, email, title }) {
   const router = useRouter();
 
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
 
   async function onClickDelete() {
     try {
@@ -20,13 +21,14 @@ export default function Edit({ id, email, title }) {
       await router.push(`/${email}`);
     } catch (error) {
       setLoading(false);
+      setOpen(false);
       console.log(error);
     }
   }
 
-  if (!loading) {
-    return (
-      <div className={styles.wrap}>
+  return (
+    <div className={styles.wrap}>
+      {!loading ? (
         <Icon
           name="edit"
           className={styles.icon}
@@ -39,19 +41,60 @@ export default function Edit({ id, email, title }) {
             });
           }}
         />
-        &nbsp;
-        <Icon
-          name="trash alternate"
-          className={styles.icon}
-          onClick={(e) => {
-            e.stopPropagation();
-            onClickDelete();
-          }}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
-    );
-  } else {
-    return <Icon name="spinner" style={{ marginTop: "12px" }} loading />;
-  }
+      ) : (
+        <Icon name="spinner" loading />
+      )}
+      &nbsp;
+      <Modal
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        trigger={
+          !loading && (
+            <Icon
+              name="trash alternate"
+              className={styles.icon}
+              onClick={(e) => e.stopPropagation()}
+            />
+          )
+        }
+      >
+        <Modal.Header>
+          <Icon name="warning circle" color="red" />
+          게시물 삭제
+        </Modal.Header>
+        <Modal.Content image>
+          <Modal.Description>
+            <Header>{title}</Header>
+            <p>정말로 삭제하시겠습니까?</p>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          {!loading ? (
+            <Button
+              color="red"
+              onClick={(e) => {
+                e.stopPropagation();
+                onClickDelete();
+              }}
+            >
+              확인
+            </Button>
+          ) : (
+            <Button color="red" loading>
+              확인
+            </Button>
+          )}
+          <Button
+            onClick={(e) => {
+              e.stopPropagation();
+              setOpen(false);
+            }}
+          >
+            취소
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
+  );
 }
