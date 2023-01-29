5f1aadcec1c78cfc3710c8144e8ccacccde6a183
yhuj79 <yu1085@naver.com>
2023-01-29 21:16:13 +0900

    Home 수정

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,45 +1,39 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
-import { Segment } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import { Button, Header, Segment } from "semantic-ui-react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const sliceEmail = session?.user.email.slice(0, 9);
+
   return (
-    <Segment inverted style={{ marginTop: 0, borderTopLeftRadius: 0 }}>
+    <Segment inverted style={{ marginTop: "-1px", borderTopLeftRadius: 0 }}>
       <Head>
         <title>Welcome! | Next-Blog</title>
       </Head>
-      <div style={{ textAlign: "center", padding: "70px" }}>
-        <h1>Welcome!</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
-          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-          aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-          imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link
-          mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
-          semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-          porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante,
-          dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
-          ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam
-          ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-          commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque
-          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-          Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-          aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
-          imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link
-          mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
-          semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-          porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante,
-          dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
-          ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam
-          ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-        </p>
-      </div>
+      {status === "unauthenticated" ? (
+        <div style={{ textAlign: "center", padding: "220px 0" }}>
+          <Header inverted as="h1">
+            Welcome!
+          </Header>
+          <Header inverted as="h3">
+            Google 계정으로 개인 블로그를 생성해 보세요
+          </Header>
+        </div>
+      ) : (
+        <div style={{ textAlign: "center", padding: "220px 0" }}>
+          <Header inverted as="h1">
+            Welcome! {sliceEmail}
+          </Header>
+          <Header inverted as="h3">
+            <Button onClick={() => router.push(`/${sliceEmail}`)}>
+              내 블로그로 이동
+            </Button>
+          </Header>
+        </div>
+      )}
     </Segment>
   );
 }
