7586d63243262367daf5b5025cc1e6e65b7c403a
유환주 <yhuj79@outlook.com>
2024-10-05 06:55:38 +0900

    sortDate import 오타 수정

--- client/pages/api/search/[email]/[input].js
+++ client/pages/api/search/[email]/[input].js
@@ -1,5 +1,5 @@
 import prisma from "../../../../lib/prisma";
-import { sortDate } from "../../utils/sortDate";
+import { sortDate } from "../../../../utils/sortDate";
 
 export default async function ApiSearch(req, res) {
   const email = req.query.email;
